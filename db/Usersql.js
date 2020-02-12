6125aaccebb3ec30a92110b432a365e066bb4f0d
MengZhaoyi <se7ven011@gmail.com>
2020-02-12 19:08:42 +0800

    Add files via upload

--- db/Usersql.js
+++ db/Usersql.js
@@ -1,7 +1,11 @@
 var UserSQL = {
-    insert: 'INSERT INTO user(email,password) VALUES(?,?)', // 插入数据
+    insertUser: 'INSERT INTO user(email,password) VALUES(?,?)', // 刷入新用户
+    updateDateforOrder: 'UPDATE user SET startdate = ?, enddate= ? WHERE email = ?',
+    updateUnavailableDates: 'UPDATE user SET unavailabledates = ? WHERE email = ?',
+    getUnavailableDates:'SELECT unavailabledates FROM user WHERE email = ?',
     drop: 'DROP TABLE user', // 删除表中所有的数据
     queryAll: 'SELECT * FROM user', // 查找表中所有数据
     getUserById: 'SELECT * FROM user WHERE email =?', // 查找符合条件的数据
 };
+
 module.exports = UserSQL;
